0e3e3b73c20fc3a0d2eaa4d1e9186172c6dada90
Shehab <shehapsalem9gmail.com>
2025-01-18 21:42:31 +0300

    productDetils screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:projec_prototyp/main.dart';
 
 void main() {
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget( MyApp());
